748aa13cff28d8eb77c19fd86f8a2bb2aea3cf6e
Christophe SAUVEUR <chsxf.pro@gmail.com>
2023-07-16 23:26:56 +0200

    🔨 Updated automation script

--- generate-docs.sh
+++ generate-docs.sh
@@ -3,21 +3,10 @@
 CWD=`pwd`
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-SCRIPT_DIR_SINGLE=$(basename "${SCRIPT_DIR}")
-SCRIPT_FILE=$(basename "${BASH_SOURCE[0]}")
 
 cd $SCRIPT_DIR
-cd ..
-TEMP_DIR_EXT="$SCRIPT_DIR-temp"
-TEMP_DIR_EXT_SINGLE="$SCRIPT_DIR_SINGLE-temp"
-mkdir $TEMP_DIR_EXT_SINGLE
 
-cd ..
-cd $SCRIPT_DIR
-mv .git $TEMP_DIR_EXT
-mv $SCRIPT_FILE $TEMP_DIR_EXT
-
-OUTPUT_PATH=$SCRIPT_DIR
+OUTPUT_PATH="$SCRIPT_DIR/docs"
 
 cd ../CiderCSSKit
 swift package \
@@ -28,11 +17,5 @@ swift package \
 	--transform-for-static-hosting \
 	--hosting-base-path CiderCSSKit \
 	--output-path $OUTPUT_PATH
-
-cd $SCRIPT_DIR
-mv $TEMP_DIR_EXT/.git .
-mv $TEMP_DIR_EXT/$SCRIPT_FILE .
-
-rmdir $TEMP_DIR_EXT
 
 cd $CWD
