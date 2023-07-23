738a16e9f1c149b6e6e77cec511f9240d1da5512
Christophe SAUVEUR <chsxf.pro@gmail.com>
2023-07-23 09:17:48 +0200

    🔨 Added script to automate documentation generation

---
+++ generate-docs.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+CWD=`pwd`
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd $SCRIPT_DIR
+
+OUTPUT_PATH="$SCRIPT_DIR-documentation/docs"
+
+swift package \
+	--allow-writing-to-directory $OUTPUT_PATH \
+	generate-documentation \
+	--target CiderCSSKit \
+	--disable-indexing \
+	--transform-for-static-hosting \
+	--hosting-base-path CiderCSSKit \
+	--output-path $OUTPUT_PATH
+
+cd $CWD
