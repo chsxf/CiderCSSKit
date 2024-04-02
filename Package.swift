19ed160abfd51766ab05a01f93a820621b4b23a5
Christophe SAUVEUR <chsxf.pro@gmail.com>
2024-04-02 14:30:58 +0200

    ➖ Removed SwiftLint as it is causing issues with apps using CiderCSSKit on other platforms than macOS

--- Package.swift
+++ Package.swift
@@ -5,26 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "CiderCSSKit",
-    platforms: [.macOS(.v12), .iOS(.v13), .tvOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "CiderCSSKit",
-            targets: ["CiderCSSKit"]),
+            targets: ["CiderCSSKit"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-        .package(url: "https://github.com/realm/SwiftLint.git", from: "0.5.4")
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CiderCSSKit",
-            dependencies: [],
-            plugins: [.plugin(name: "SwiftLintPlugin", package: "SwiftLint")]),
+            dependencies: []),
         .testTarget(
             name: "CiderCSSKitTests",
             dependencies: ["CiderCSSKit"],
@@ -36,7 +33,6 @@ let package = Package(
                 .copy("ParserCustomTests.ckcss"),
                 .copy("ParserInvalidCustomTests.ckcss"),
                 .copy("ParserRuleBlockTests.ckcss")
-            ],
-            plugins: [.plugin(name: "SwiftLintPlugin", package: "SwiftLint")]),
+            ])
     ]
 )
